5005046ab9ede9bc4baf229d8c5fe156b628374f
Duda <andreyduda88@gmail.com>
2018-02-07 21:34:21 +0200

    Route for landing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+Route::group(['middleware'=>'web'], function(){
+
+    Route::match(['get','post'], '/', ['uses'=>'IndexController@execute', 'as'=>'home']);
+    Route::get('/page/{alias}',['uses'=>'PagController@execute', 'as'=>'page']);
+
+    Route::auth();
+});
+
+Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function(){
+
+    Route::get('/', function(){
+
+    });
+    Route::groupe(['prefix'=>'page'], function(){
+
+        Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
+        Route::match(['get', 'post'], '/add', ['uses'=>'PagesController@execute', 'as'=>'pagesAdd']);
+        Route::match(['get','post', 'delate'], '/edit/{page}', ['uses'=>'PagesController@execute', 'as'=>'pagesEdit']);
+
+    });
+
+    Route::groupe(['prefix'=>'portfolios'], function(){
+
+        Route::get('/', ['uses'=>'PortfolioController@execute', 'as'=>'portfolio']);
+        Route::match(['get', 'post'], '/add', ['uses'=>'PortfolioController@execute', 'as'=>'portfolioAdd']);
+        Route::match(['get','post', 'delate'], '/edit/{portfolio}', ['uses'=>'PortfolioController@execute', 'as'=>'portfolioEdit']);
+
+    });
+    Route::groupe(['prefix'=>'services'], function(){
+
+        Route::get('/', ['uses'=>'ServiceController@execute', 'as'=>'service']);
+        Route::match(['get', 'post'], '/add', ['uses'=>'ServiceController@execute', 'as'=>'serviceAdd']);
+        Route::match(['get','post', 'delate'], '/edit/{portfolio}', ['uses'=>'ServiceController@execute', 'as'=>'serviceEdit']);
+
+    });
 });
